46b465f5154bbcb6594ee153a05b00892b6e7a04
fatihyanik <fatihyanik07@gmail.com>
2021-11-23 22:19:48 +0100

    I added animation

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import "./app.css";
+import Trivia from "./components/Trivia"
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(5)
@@ -27,7 +28,9 @@ function App() {
        <div className="top">
          <div className="timer">30</div>
        </div>
-       <div className="bottom">questions and answers</div>
+       <div className="bottom">
+         <Trivia />
+        </div>
      </div>
      <div className="pyramid">
        <ul className="moneyList">
---
+++ quiz/src/components/Trivia.jsx
@@ -0,0 +1,15 @@
+export default function Trivia() {
+  return (
+    <div className="trivia">
+      <div className="question">What's the best youtube channel</div>
+      <div className="answers">
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+        <div className="answer">Lama Dev</div>
+      </div>
+    </div>
+  );
+}
+
+
