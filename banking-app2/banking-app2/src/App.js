1942419ef7b1e29555a2946d5d1816526482ea3d
fatihyanik <fatihyanik07@gmail.com>
2021-11-23 21:10:17 +0100

    I added banking-app2

---
+++ banking-app2/banking-app2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banking from "./Banking";
+
+function App() {
+  return (
+    <div className="App">
+     <Banking />
+    </div>
+  );
+}
+
+export default App;
