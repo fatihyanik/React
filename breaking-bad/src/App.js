e88d5a74959ecbe2f1578095f28389cd74531e66
fatihyanik <fatihyanik07@gmail.com>
2021-12-08 21:52:45 +0100

    I added CharacterGrid.js

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+
 
 function App() {
+
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=>{
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  },[])
+
+
+
   return (
     <div className="container">
-      hello
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
