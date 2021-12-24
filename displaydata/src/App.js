a633dd7e514928d9c6e88fb543abd70207ce09c4
fatihyanik <fatihyanik07@gmail.com>
2021-12-24 11:38:41 +0100

    i added photo

--- displaydata/src/App.js
+++ displaydata/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-
 const user = {
   name: "Fatih",
   location: "Leipzig",
@@ -8,38 +7,27 @@ const user = {
   likes: "Coding into the wee hours of the morning",
   twitterUsername: "fatih_twitter",
   avatar:
-    "https://scotch-res.cloudinary.com/image/upload/v1556479698/xRZsnhr0_400x400_cpyg2t.png",
+    "https://i1.trekearth.com/photos/22441/a08011_0440.jpg",
 };
 
-function App() {
+
+export default function App() {
+  
+  const url = `https://twitter.com/${user.twitterUsername}`;
+
   return (
     <div className="App">
-      <div className="user-deets">
-        <h3>{user.name}</h3>
 
-        <p>
-          <strong>Location</strong>
-          {user.location}
-        </p>
-        <p>
-          <strong>Eats</strong>
-          {user.foodType}
-        </p>
-        <p>
-          <strong>Age</strong>
-          {user.age}
-        </p>
-        <p>
-          <strong>Likes</strong>
-          {user.likes}
-        </p>
-        <p>
-          <strong>Twitter</strong>
-          <a href={`https://twitter.com/${user.twitterUsername}`}>@{user.twitterUsername}</a>
-        </p>
-      </div>
+    <div className="user-deets">
+      <img src={user.avatar} alt={user.name} />
+      <h3><a href={url}>{user.name}</a></h3>
+      <p><strong>Location</strong>{user.location}</p>
+      <p><strong>Eats</strong>{user.foodType}</p>
+      <p><strong>Age</strong>{user.age}</p>
+      <p><strong>Likes</strong>{user.likes}</p>
+      <p><strong>Twitter</strong><a href={url}>@{user.twitterUsername}</a></p>
+    </div>
+
     </div>
   );
 }
-
-export default App;
