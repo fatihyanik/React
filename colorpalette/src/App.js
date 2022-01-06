a39d161abc4ffb36e3996632545d1d286aefe1a0
fatihyanik <fatihyanik07@gmail.com>
2022-01-06 20:56:44 +0100

    I added color-structure

---
+++ colorpalette/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {CopyToClipboard} from "react-copy-to-clipboard";
+
+function App() {
+
+  const colors = [
+    "#9253a1",
+    
+  ]
+
+
+
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
