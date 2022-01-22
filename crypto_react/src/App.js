411e9828d77e43e35baa6321791514e96fe5db24
fatihyanik <fatihyanik07@gmail.com>
2022-01-22 17:19:32 +0100

    i added app.css and i created Coin.js

--- crypto_react/src/App.js
+++ crypto_react/src/App.js
@@ -1,22 +1,33 @@
-import './App.css';
-import {useEffect, useState} from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
 import Axios from "axios";
+import Coin from "./components/Coin";
 
 function App() {
-
   const [listOfCoins, setListOfCoins] = useState([]);
 
-  useEffect(() =>{
-   Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then((response) =>{
-     setListOfCoins(response.data.coins)
-   })
-  }, [])
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+      }
+    );
+  }, []);
   return (
     <div className="App">
       <div className="cryptoHeader"></div>
-      <div className="cryptoDisplay">{listOfCoins.map((coin)=>{
-        return <h1>{coin.name}</h1>
-      })}</div>
+      <div className="cryptoDisplay">
+        {listOfCoins.map((coin, index) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
