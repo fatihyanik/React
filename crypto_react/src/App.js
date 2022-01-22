b43b00627a784cf8f6dd7d4855c31731c58e7c93
fatihyanik <fatihyanik07@gmail.com>
2022-01-22 17:07:53 +0100

    I used axios and i got bitcoin datas

--- crypto_react/src/App.js
+++ crypto_react/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import Axios from "axios";
 
 function App() {
+
+  const [listOfCoins, setListOfCoins] = useState([]);
+
+  useEffect(() =>{
+   Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then((response) =>{
+     setListOfCoins(response.data.coins)
+   })
+  }, [])
   return (
     <div className="App">
- 
+      <div className="cryptoHeader"></div>
+      <div className="cryptoDisplay">{listOfCoins.map((coin)=>{
+        return <h1>{coin.name}</h1>
+      })}</div>
     </div>
   );
 }
