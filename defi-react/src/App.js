b74f64fe1826a0eb15431f88afd98a68094f152e
fatihyanik <fatihyanik07@gmail.com>
2022-01-23 16:05:21 +0100

    i added hero css

--- defi-react/src/App.js
+++ defi-react/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar />
+        <Hero />
     </div>
   );
 }
