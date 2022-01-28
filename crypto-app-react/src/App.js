87b89486aff099210ed51fe9821a09dfb67aa25e
fatihyanik <fatihyanik07@gmail.com>
2022-01-28 20:23:48 +0100

    i added CoinItem and Coins components

--- crypto-app-react/src/App.js
+++ crypto-app-react/src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
 import './App.css';
+import Coins from "./components/Coins";
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+    }).catch((error)=>{
+      console.log(error);
+    })
+  },[]);
+
   return (
-    <div className="App">
-    
-    </div>
+    <>
+    <Coins coins={coins} />
+    </>
   );
 }
 
