65330da14c5420bedb8dab7009e20499e0416a76
fatihyanik <fatihyanik07@gmail.com>
2022-02-05 12:00:11 +0100

    I added food components

--- food-react-app/src/App.js
+++ food-react-app/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Food from './components/food/Food';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
+
+// Import images
+import Craft1 from './assets/craft1.jpg';
+import Craft2 from './assets/craft2.jpg';
+import Chicken from './assets/chicken.jpg';
+import Burger from './assets/burger.jpg';
+import HeroBg from './assets/hero-bg.jpg';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Food bgImg1={Craft1} bgImg2={Craft2} />
+      <Food bgImg={Chicken}/>
+      <Food bgImg={Burger}/>
+
     </>
   );
 }
---
+++ food-react-app/src/components/food/Food.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FoodStyles.css';
+
+function Food(props) {
+    return (
+        <div style={{background:`url('${props.bgImg}') no-repeat center center/cover`}} className="food">
+            <div className="container">
+                <div style={{background:`url('${props.bgImg1}') no-repeat center center/cover`}} className="left"></div>
+                <div style={{background:`url('${props.bgImg2}') no-repeat center center/cover`}} className="right"></div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Food
