eae9ac2a93177aca61d960dddf96dd8a412f2fb2
fatihyanik <fatihyanik07@gmail.com>
2022-02-05 11:46:09 +0100

    i added hero components

--- food-react-app/src/App.js
+++ food-react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import Hero from './components/hero/Hero';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
