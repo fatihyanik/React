14f59ee23e43dfb73807fedf0646496c2ee72ded
fatihyanik <fatihyanik07@gmail.com>
2022-05-13 10:00:38 +0200

    deleted container folder

---
+++ banking-app-1/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Banking from './Banking'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Banking />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
