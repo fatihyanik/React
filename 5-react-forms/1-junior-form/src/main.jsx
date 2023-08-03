7ff4850768faffced26c098ead4687a93c9b785f
MuratFatih_FEdev_ _ <you@example.com>
2023-08-03 23:13:22 +0200

    added basic form

---
+++ 5-react-forms/1-junior-form/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import MyForm from './MyForm'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MyForm />
+  </React.StrictMode>,
+)
