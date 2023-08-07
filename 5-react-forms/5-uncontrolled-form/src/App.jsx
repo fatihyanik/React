866f36537bde79b189f35c0093d13086c3135891
MuratFatih_FEdev_ _ <you@example.com>
2023-08-07 23:07:21 +0200

    added uncontrolled form

---
+++ 5-react-forms/5-uncontrolled-form/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import './index.css'; // CSS dosyasını import ediyoruz
+
+const UncontrolledForm = () => {
+  // Input değerine erişmek için useRef kullanıyoruz
+  const inputRef = useRef();
+
+  // Form gönderildiğinde çalışacak fonksiyon
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Gönderilen Değer:', inputRef.current.value);
+  };
+
+  return (
+    <div className="form-container"> {/* CSS sınıfını ekledik */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Kontrolsüz Form:
+          <input type="text" ref={inputRef} />
+        </label>
+        <button type="submit">Gönder</button>
+      </form>
+    </div>
+  );
+};
+
+export default UncontrolledForm;
